f7a53c54d6343647b241ff3ef6b7b0562c666685
Theresia <e1375477@u.nus.edu>
2025-01-29 21:30:03 +0800

    V1.0 - Cleaned up App.jsx & Moved intdex.html to src - Theresia

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,3 @@
-// export default App;
-// import Header from './components/Header';
-// import SearchBar from './components/SearchBar';
-// import ToiletList from './components/ToiletList';
-// import Footer from './components/Footer';
-
-// function App() {
-//   return (
-//     <div className="bg-white p-0 w-screen min-h-screen flex-col">
-//       <Header />
-//       <SearchBar />
-//       <ToiletList />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
 import { SearchProvider } from './SearchContext'; // Global searching process
 import { OptionsProvider } from './OptionsContext';
 import Header from './components/Header';
